c915ef5c08ee7f8da219c62d51f01ecddf9e3dd3
Nikhil Goli <golinikhil1@gmail.com>
2025-05-17 19:02:45 -0400

    fixed api credentials issue

--- frontend/lib/auth.ts
+++ frontend/lib/auth.ts
@@ -10,6 +10,7 @@ export const api = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
+  withCredentials: true // Enable credentials for all requests
 })
 
 interface AuthContextType {
